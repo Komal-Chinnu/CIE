1f09580d881882f4a38550935326a6ecad635af1
komalpawar2k13@gmail.com <komalpawar2k13@gmail.com>
2025-04-15 09:59:27 +0530

    File saved

---
+++ calculate.java
@@ -0,0 +1,15 @@
+package CIE;
+
+public class calculate {
+    public static void main(String[] args) {
+        
+        int a= 10;
+        int b=5;
+
+        System.out.println("Addition Result :" +(a+b));
+        System.out.println("Addition Result :" +(a-b));
+        System.out.println("Addition Result :" +(a*b));
+        System.out.println("Addition Result :" +(a/b));
+    }
+    
+}
